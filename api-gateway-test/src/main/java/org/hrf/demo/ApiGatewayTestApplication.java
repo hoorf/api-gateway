c9967e5766fe7dd6355c64bcc5db65ea05bdeb5d
ruifengho <ruifengho@163.com>
2018-02-06 10:11:57 +0800

    添加测试例子

---
+++ api-gateway-test/src/main/java/org/hrf/demo/ApiGatewayTestApplication.java
@@ -0,0 +1,27 @@
+package org.hrf.demo;
+
+import org.hrf.gateway.core.ApiContext;
+import org.hrf.gateway.core.ApiServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayTestApplication {
+
+	@Bean
+	public ServletRegistrationBean apiServlet() {
+		ServletRegistrationBean result = new ServletRegistrationBean(new ApiServlet());
+		result.addUrlMappings("/api");
+		return result;
+	}
+
+	static ApplicationContext context;
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(ApiGatewayTestApplication.class, args);
+		ApiContext.init(context);
+	}
+}
---
+++ api-gateway-test/src/main/java/org/hrf/demo/test/TestService.java
@@ -0,0 +1,14 @@
+package org.hrf.demo.test;
+
+import org.hrf.gateway.core.annotation.MappingApi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestService {
+
+	@MappingApi("org.hrf.user")
+	public String getUser(String id) {
+		return "id:"+id;
+	}
+
+}
